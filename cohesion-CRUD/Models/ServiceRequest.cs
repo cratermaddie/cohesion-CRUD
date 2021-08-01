262ee0a6f7fa51a3d8ea0e823e1b2e0eb1644579
Madison Crater <cratermaddie@gmail.com>
2021-08-01 17:01:20 -0500

    Added ServiceRequest model

---
+++ cohesion-CRUD/Models/ServiceRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cohesion_CRUD.Models
+{
+	public class ServiceRequest
+	{
+		public Guid id { get; } = new Guid();
+		public String buildingCode { get; set; }
+		public String description { get; set; }
+		public CurrentStatus currentStatus { get; set; }
+		public String createdBy { get; set; }
+		public DateTime createdDate { get; set; }
+		public String lastModifiedBy { get; set; }
+		public DateTime lastModifiedDate { get; set; }
+	}
+
+	public enum CurrentStatus
+	{
+		NotApplicable,
+		Created,
+		InProgress,
+		Complete,
+		Canceled
+	}
+}
