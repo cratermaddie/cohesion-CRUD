4f0e231c108e208238e4c99abc92cf4c52f6648c
Madison Crater <cratermaddie@gmail.com>
2021-08-01 19:22:45 -0500

    Remove unused using statements

--- cohesion-CRUD/Models/ServiceRequest.cs
+++ cohesion-CRUD/Models/ServiceRequest.cs
@@ -1,10 +1,6 @@
 ﻿
-using Newtonsoft.Json.Converters;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace cohesion_CRUD.Models
 {
